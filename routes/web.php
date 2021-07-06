9d8acfe28c64d1b5cd4909f740b37fb4c7bc2244
CrisPro2020 <cristianbernal212@gmail.com>
2021-07-06 11:24:48 -0500

    Se agrego una nueva ruta llamada SERVICIOS

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('inicio');
 
 Route::view('quienes-somos', 'quienes-somos')->name('quienes-somos');
+Route::view('servicios', 'servicios')->name('servicios');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
