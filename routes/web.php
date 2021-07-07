7b6bd8b69ebc7c6fbbea9700de0357b57b05bd99
CrisPro2020 <cristianbernal212@gmail.com>
2021-07-06 20:54:06 -0500

    Se creo la ruta del PORTAFOLIO

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', HomeController::class)->name('inicio');
 
 Route::view('quienes-somos', 'quienes-somos')->name('quienes-somos');
 Route::view('servicios', 'servicios')->name('servicios');
+Route::view('portafolio', 'portafolio')->name('portafolio');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
